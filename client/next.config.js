bfcd544432836a6e471db28aec4e27022720eee7
iimagii <mag_1@ukr.net>
2023-05-08 18:30:22 +0300

    video-cinema-next

---
+++ client/next.config.js
@@ -0,0 +1,23 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  poweredByHeader: false,//чтобы не было видно что наш сайт сделан на NEXT
+  env: {
+    APP_URL: process.env.NEXT_PUBLIC_URL,
+    APP_ENV: process.env.NEXT_PUBLIC_ENV,
+  },
+  //перезаписываем пути для бекенда
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `http://localhost:4200/api/:path*`,
+      },
+      {
+        source: '/uploads/:path*',
+        destination: `http://localhost:4200/uploads/:path*`,
+      },
+    ]
+  },
+}
+
+module.exports = nextConfig
